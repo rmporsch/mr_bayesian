4fa6d754874f921124a8fbe3da8d83c744374616
baselmans <b.m.l.baselmans@vu.nl>
2017-08-28 17:52:03 +0800

    Update Loop_Bayesian_SNPdata.R

--- Loop_Bayesian_SNPdata.R
+++ Loop_Bayesian_SNPdata.R
@@ -3,6 +3,7 @@ library(bnlearn)
 library(compare)
 library(plyr)
 library(dplyr)
+library(gdata)
 
 Sim <- list()
 Sim1 <- list()
@@ -33,18 +34,30 @@ Total_score <- list()
 
 ###SNPDATA
 
-setwd(dir = "C:/Users/bbs520/Dropbox/Mendelian_Randomization/Bayesian Network/BayesianNetworkLearn/SIM/SIM3")
-
-
+setwd(dir = "C:/Users/bbs520/Dropbox/Mendelian_Randomization/Bayesian Network/BayesianNetworkLearn/SIM/SIM4")
+##### Preperate the data for analysis
 
 for (i in 1:500){
-  bl1 <- matrix(c("t1","t1","t1","t1","t1","t1","t1","t1","t1","t1","t1","t1","t1","t1","t1", "snp1.1" ,"snp1.2","snp1.3","snp1.4","snp1.5","snp2.1" ,"snp2.2","snp2.3","snp2.4","snp2.5","snp3.1" ,"snp3.2","snp3.3","snp3.4","snp3.5"),ncol=2)
-  bl2 <- matrix(c("t2","t2","t2","t2","t2","t2","t2","t2","t2","t2","t2","t2","t2","t2","t2", "snp1.1" ,"snp1.2","snp1.3","snp1.4","snp1.5","snp2.1" ,"snp2.2","snp2.3","snp2.4","snp2.5","snp3.1" ,"snp3.2","snp3.3","snp3.4","snp3.5"),ncol=2)
-  bl3 <- matrix(c("t3","t3","t3","t3","t3","t3","t3","t3","t3","t3","t3","t3","t3","t3","t3", "snp1.1" ,"snp1.2","snp1.3","snp1.4","snp1.5","snp2.1" ,"snp2.2","snp2.3","snp2.4","snp2.5","snp3.1" ,"snp3.2","snp3.3","snp3.4","snp3.5"),ncol=2)
+  
+  snp <- c("snp1.1" ,"snp1.2","snp1.3","snp1.4","snp1.5",
+           "snp2.1","snp2.2","snp2.3","snp2.4","snp2.5",
+           "snp3.1" ,"snp3.2","snp3.3","snp3.4","snp3.5")
+  t1 <- c("t1")
+  bl1 <- t(as.matrix(rbind(t1,snp)))
+  colnames(bl1) <- NULL
+  t2 <- c("t2")
+  bl2 <- t(as.matrix(rbind(t2,snp)))
+  colnames(bl2) <- NULL
+  t3 <- c("t3")
+  bl3 <- t(as.matrix(rbind(t3,snp)))
+  colnames(bl3) <- NULL
+  
   bl <- rbind(bl1,bl2,bl3)
+  
   
   #perform the BNlearning algorithm to all simulated data
   temp <- read.delim(paste0('simData_',i, '.txt'), header=T, sep = " ")
+  #temp <- temp[4:18] <- lapply(temp[4:18], as.numeric)
   temp$snp1.1 <- as.numeric(temp$snp1.1)
   temp$snp1.2 <- as.numeric(temp$snp1.2)
   temp$snp1.3 <- as.numeric(temp$snp1.3)
@@ -63,8 +76,8 @@ for (i in 1:500){
   temp$snp3.4 <- as.numeric(temp$snp3.4)
   temp$snp3.5 <- as.numeric(temp$snp3.5)
   
-  
   Sim[[i]] <- hc(temp, blacklist = bl)
+
   
   #Select only the causal paths
   Sim1[[i]] <- lapply(Sim[[i]]$nodes, function(k) k$children)
@@ -74,6 +87,8 @@ for (i in 1:500){
   df.true.model[[i]]$value <- NULL
   colnames(df.true.model[[i]])[1] <- "from"
   colnames(df.true.model[[i]])[2] <- "to"
+  
+  
   #Unlist causal arcs simulate data to data frame
   df.causal.con[[i]] <- data.frame(from = rep(names(Sim1[[i]]), sapply(Sim1[[i]], length)),
                                    to = unlist(Sim1[[i]]))
@@ -102,41 +117,32 @@ for (i in 1:500){
   Score_compare_tm_sc_t1t2t3[[i]] <- nrow(comparisson_3[[i]])
   Score_compare_tm_sa_t1t2t3[[i]] <- nrow(comparisson_4[[i]])
   
-  Total_score[[i]] <- cbind(score_true_model[[i]],score_causal.con[[i]],score_all.con[[i]],Score_compare_tm_sc[[i]],
-                            Score_compare_tm_ap[[i]],Score_true_t1t2t3[[i]],Score_sc_t1t2t3[[i]],Score_sa_t1t2t3 [[i]],
-                            Score_compare_tm_sc_t1t2t3[[i]],Score_compare_tm_sa_t1t2t3[[i]])
+  Total_score[[i]] <- cbind(Score_true_t1t2t3[[i]],Score_sc_t1t2t3[[i]],
+                            Score_compare_tm_sc_t1t2t3[[i]])
   
   df.Total_score <- ldply(Total_score, data.frame)
-  colnames(df.Total_score)[1] <- "True_Model"
-  colnames(df.Total_score)[2] <- "Sim_causal_path"
-  colnames(df.Total_score)[3] <- "Sim_all_paths"
-  colnames(df.Total_score)[4] <- "True_vs_Sim_Causal"
-  colnames(df.Total_score)[5] <- "True_vs_Sim_all"
-  colnames(df.Total_score)[6] <- "True_t1t2t3"
-  colnames(df.Total_score)[7] <- "Sim_causal_t1t2t3"
-  colnames(df.Total_score)[8] <- "Sim_all_t1t2t3"
-  colnames(df.Total_score)[9] <- "True_vs_Sim_Causal_t1t2t3"
-  colnames(df.Total_score)[10] <- "True_vs_Sim_all_t1t2t3"
+
+  colnames(df.Total_score)[1] <- "True_t1t2t3"
+  colnames(df.Total_score)[2] <- "Sim_causal_t1t2t3"
+  colnames(df.Total_score)[3] <- "True_vs_Sim_Causal_t1t2t3"
+ 
+  
 }   
 
-print(sum(df.Total_score$True_vs_Sim_Causal)) / (sum(df.Total_score$True_Model)) *100
-print(sum(df.Total_score$True_vs_Sim_all)) / (sum(df.Total_score$True_Model)) *100
-print(sum(df.Total_score$True_vs_Sim_Causal_t1t2t3)) / (sum(df.Total_score$True_t1t2t3)) *100
-print(sum(df.Total_score$True_vs_Sim_all_t1t2t3)) / (sum(df.Total_score$True_t1t2t3)) *100
-
-df.Total_score <- add_rownames(df.Total_score, "Model_nr")
+df.Total_score <-add_rownames(df.Total_score, "Model_nr")
 head(df.Total_score)
 
-#mis identified missing arcs
+## Underestimations
 
-missing_arcs <- df.Total_score[which(df.Total_score$True_t1t2t3!=df.Total_score$True_vs_Sim_Causal_t1t2t3),]
-head(missing_arcs)
-missing_arcs1 <- as.data.frame(cbind(missing_arcs$Model_nr,missing_arcs$True_t1t2t3,missing_arcs$True_vs_Sim_Causal_t1t2t3))
-head(missing_arcs1)
+underestimations <- df.Total_score[which(df.Total_score$True_t1t2t3!=df.Total_score$True_vs_Sim_Causal_t1t2t3),]
+underestimations1 <- as.data.frame(cbind(underestimations$Model_nr,underestimations$True_t1t2t3,underestimations$True_vs_Sim_Causal_t1t2t3))
 
-colnames(missing_arcs1)[1] <- "model_nr"
-colnames(missing_arcs1)[2] <- "True_model"
-colnames(missing_arcs1)[3] <- "Sim_causal"
+colnames(underestimations1)[1] <- "model_nr"
+colnames(underestimations1)[2] <- "True_model"
+colnames(underestimations1)[3] <- "Sim_causal"
+
+
+## Overestimation
 
 overestimations <- df.Total_score[which(df.Total_score$Sim_causal_t1t2t3 > df.Total_score$True_t1t2t3),]
 overestimations1 <- as.data.frame(cbind(overestimations$Model_nr,overestimations$True_t1t2t3,overestimations$Sim_causal_t1t2t3))
@@ -144,8 +150,14 @@ overestimations1 <- as.data.frame(cbind(overestimations$Model_nr,overestimations
 colnames(overestimations1)[1] <- "model_nr"
 colnames(overestimations1)[2] <- "True_model"
 colnames(overestimations1)[3] <- "Sim_causal"
-head(overestimations1)
 
-true_t1t2t3[[3]]
-sc_t1t2t3[[3]]
+## Correct
+
+Correct <-  df.Total_score[which(df.Total_score$True_t1t2t3==df.Total_score$Sim_causal_t1t2t3),]
+Correct1 <- as.data.frame(cbind(Correct$Model_nr,Correct$True_t1t2t3,Correct$Sim_causal_t1t2t3))
+
+
+rm(list=setdiff(ls(), "Correct1","underestimations1", "overestimations1"))
+
+keep(Correct1,underestimations1, overestimations1, sure=TRUE)
 
