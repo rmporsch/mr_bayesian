11624fff60bb8957904a7af013c8e631b699948d
Robert Milan Porsch <robert.porsch@hku.hk>
2017-08-29 14:05:55 +0800

    added mrbase testing

--- functions.r
+++ functions.r
@@ -30,7 +30,7 @@ prs <- function (G, sum.stat, p) {
   return(out)
 }
 
-gwas <- function (y, X, covar=NULL) {
+gwas <- function (y, X, bim, covar=NULL, label=NULL) {
   est.effect <- matrix(0, ncol=4, nrow=ncol(X))
   if (is.null(covar)) {
     for (i in 1:ncol(X)) {
@@ -42,7 +42,12 @@ gwas <- function (y, X, covar=NULL) {
     }
   }
   est.effect <- as.data.frame(est.effect)
-  names(est.effect) <- c("beta", "SE", "tvalue", "p")
+  names(est.effect) <- c("beta", "se", "tvalue", "pval")
+  est.effect$SNP  <- bim$V2
+  est.effect$effect_allele  <- bim$V5
+  est.effect$other_allele  <- bim$V6
+
+  if(!is.null(label)) est.effect$Phenotype <- label
   return(est.effect)
 }
 
@@ -74,7 +79,7 @@ define.causal <- function (p, effectDist) {
   return(l)
 }
 
-simulate.complex <- function (n, p, effectmat, dd, phenoeffect, wished.effect=0.6) {
+simulate.causal.prs <- function (n, p, effectmat, dd, phenoeffect, wished.effect=0.6) {
   ca <- define.causal(p, effectmat)
   ef1 <-rep(0, p)
   ef2 <-rep(0, p) 
@@ -114,7 +119,7 @@ qq.gwas <- function (p) {
 }
 
 make.arcs <- function (pheno.effectmat) {
-  base <- matrix(c(paste0('prs',1:3), paste0('t', 1:3)), ncol=2)
+  base <- matrix(c(paste0('snp',1:3), paste0('t', 1:3)), ncol=2)
   causal.path <- which(pheno.effectmat!=0, T)
   causal.path <- matrix(c(colnames(pheno.effectmat)[causal.path[,2]],
                         rownames(pheno.effectmat)[causal.path[,1]]), ncol=2)
@@ -122,11 +127,126 @@ make.arcs <- function (pheno.effectmat) {
 }
 
 generate.random.causes <- function(effect.size) {
-  effect.size <- c(0, 0.3, 0.7, 0.3,0)
   pheno.effectmat <- matrix(0, nrow=3, ncol=3)
   pheno.effectmat[lower.tri(pheno.effectmat)] <- sample(effect.size, 3)
   diag(pheno.effectmat) <- NA
   colnames(pheno.effectmat) <- paste0('t', 1:3)
   rownames(pheno.effectmat) <- paste0('t', 1:3)
   return(pheno.effectmat)
+}
+
+simulate.causal.snp <- function (n, p, effectmat, dd, phenoeffect, wished.effect=0.6, bim, dogwas=F) {
+  effectmat <- genetic.effectmat
+  phenoeffect <- pheno.effectmat
+  ca <- define.causal(p, effectmat)
+  ef1 <-rep(0, p)
+  ef2 <-rep(0, p) 
+  ef3 <-rep(0, p) 
+  efca <- list(ef1, ef2, ef3)
+  for (item in 1:length(efca)) {
+    efca[[item]][ca[[item]]] <- 1 
+    efca[[item]][ca[[item]]] <- efca[[item]][ca[[item]]] /
+    (sum(efca[[item]][ca[[item]]])/wished.effect)
+  }
+
+  t1  <-  1*(dd%*%efca[[1]]) + rnorm(n, 0, sqrt(1-wished.effect))
+  t2  <-  1*(dd%*%efca[[2]]) + phenoeffect[2,1]*t1 + rnorm(n, 0, sqrt(1-wished.effect))
+  t3  <-  1*(dd%*%efca[[3]]) + phenoeffect[3,1]*t1 + phenoeffect[3,1]*t2 +
+  rnorm(n, 0, sqrt(1-wished.effect))
+
+
+  if(dogwas==T) {
+  gwas1 <- gwas(t1, dd, bim, label="T1")
+  gwas2 <- gwas(t2, dd, bim, label="T2")
+  gwas3 <- gwas(t3, dd, bim, label="T3")
+  gwas1$causal <- 0
+  gwas1$causal[ca[[1]]] <- 1
+  gwas2$causal <- 0
+  gwas2$causal[ca[[2]]] <- 1
+  gwas3$causal <- 0
+  gwas3$causal[ca[[3]]] <- 1
+  gwas_results <- rbind(gwas1, gwas2, gwas3)
+  } else {
+    gwas_results <- NULL
+  }
+
+  snp1 <- as.data.frame(dd[,ca[[1]]])
+  names(snp1) <- paste0('snp1.', 1:ncol(snp1))
+  snp2 <- as.data.frame(dd[,ca[[2]]])
+  names(snp2) <- paste0('snp2.', 1:ncol(snp2))
+  snp3 <- as.data.frame(dd[,ca[[3]]])
+  names(snp3) <- paste0('snp3.', 1:ncol(snp3))
+
+  output <- data.frame(t1, t2, t3,
+                       snp1, snp2, snp3)
+  outout <- list(output, gwas_results)
+  return(outout)
+}
+
+
+test.mr <- function (sim) {
+  possibilities <- list(c("T1", "T2"), c("T1", "T3"), c("T2", "T3"))
+  out <- list() 
+
+  u <- 1 
+  for (item in possibilities) {
+    expo <- sim %>%
+    filter(Phenotype==item[1], causal==1) %>%
+    format_data(., type='exposure')
+
+    outcome <-  sim %>%
+    filter(Phenotype==item[2]) %>%
+    format_data(., type='outcome', phenotype_col="Phenotype")
+
+    dat <- harmonise_data(exposure_dat = expo, 
+                          outcome_dat = outcome, action=1)
+    out[[u]] <- mr(dat, method_list="mr_ivw")
+    u <- u + 1
+  }
+
+  out <- do.call('rbind', out)
+  out <- out %>% 
+  select(exposure, outcome, b, se, pval) %>%
+  mutate(causal=pval<=(0.05/length(possibilities)))
+
+  return(out)
+}
+
+compare.outcomes <- function (real, estimated, causal='causal') {
+  real <- arcs[!is.na(arcs$value),c("from", "to")]
+  estimated <- outcome[outcome[[causal]], c("from", "to")]
+  real <- mutate_each(real, funs(tolower))
+  estimated <- mutate_each(estimated, funs(tolower))
+
+
+  if(nrow(real)==0) names(real) <- "empty"
+  if(nrow(estimated)==0) names(estimated) <- "empty"
+
+  if (nrow(real)==0 & nrow(estimated) > 0) {
+    overestimated <- estimated 
+    not.estimated <- NULL
+  }
+
+  if (nrow(real)>0 & nrow(estimated) == 0) {
+    not.estimated <- real 
+    overestimated  <- NULL
+  }
+
+  if (nrow(real)>0 & nrow(estimated) > 0) {
+    not.estimated <- anti_join(real, estimated)
+    overestimated <- anti_join(estimated, real)
+  }
+  return(list("not.estimated"=not.estimated, "overestimated"=overestimated)) 
+}
+
+check.mr.performance <- function (k) {
+  out <- vector('numeric', length=2)
+  for (item in 1:length(k)) {
+    if(is.null(k[item][[1]])) { 
+      out[item] <- 0
+    } else {
+      out[item] <- nrow(k[item][[1]])
+    }
+  }
+  return(out)
 }
--- simulate.r
+++ simulate.r
@@ -8,7 +8,7 @@ Tim.load(mysimtools) # Tim.load is basically just devtools::load <- all() but ap
 useWTCCC() # This runs bfile('/path/to/WTCCC bfile')
 # bfile() specifies the --bfile to use with plink
 set.seed(42)
-n  <- 2000; p <- 2000
+n  <- 5000; p <- 200
 pdfile <- subsample(n=n, p=p)
 dd <- load.to.R(pdfile)
 
@@ -20,13 +20,36 @@ bim <- read.table(paste0(pdfile, ".bim"))
 #'#################'
 
 # proportion of SNPs with an effect
-genetic.effectmat <- matrix(c(0.01, 0.000, 0.000,
+genetic.effectmat <- matrix(c(0.010, 0.000, 0.000,
                               0.000, 0.01, 0.000,
                               0.000, 0.000, 0.01), nrow=3, ncol=3, byrow=T)
+rownames(genetic.effectmat) <- paste0('t',1:3)
+colnames(genetic.effectmat) <- paste0('t',1:3)
 
 effect <- c(rep(0, 6), runif(20,0,0.8))
+effect <- c(rep(0, 6), rep(0.4, 4))
 library(tidyverse)
-for (item in 1:1000) {
+#for (item in 1:100) {
+#  pheno.effectmat <- generate.random.causes(effect)
+#  report.arcs <- data.frame(pheno.effectmat, 'to'=rownames(pheno.effectmat))
+#  report.arcs <- gather(report.arcs, 'from', 'value', -to) %>%
+#    filter(value!=0) %>%
+#    select(from, to, value)
+#  arcs <- make.arcs(pheno.effectmat)
+#  arcs <- as.data.frame(arcs)
+#  names(arcs) <- c('from', 'to')
+#  arcs <- merge(arcs, report.arcs, all.x=T )
+#
+#  out <- simulate.causal.prs(n, p, effectmat, dd, pheno.effectmat)
+#  write.table(out, paste0('simulation/simData_', item, '.txt'), row.names=F, quote=F)
+#  write.table(arcs, paste0('simulation/simSolution_', item, '.txt'), row.names=F, quote=F)
+#}
+#zip('simulations_prs.zip', 'simulation')
+#system('rm -r simulation/')
+
+
+dir.create('simulation')
+for (item in 1:200) {
   pheno.effectmat <- generate.random.causes(effect)
   report.arcs <- data.frame(pheno.effectmat, 'to'=rownames(pheno.effectmat))
   report.arcs <- gather(report.arcs, 'from', 'value', -to) %>%
@@ -37,8 +60,11 @@ for (item in 1:1000) {
   names(arcs) <- c('from', 'to')
   arcs <- merge(arcs, report.arcs, all.x=T )
 
-  out <- simulate.complex(n, p, effectmat, dd, pheno.effectmat)
-  write.table(out, paste0('simulation/simData_', item, '.txt'), row.names=F, quote=F)
+  out <- simulate.causal.snp(n, p, genetic.effectmat, dd, pheno.effectmat, 0.8, bim, dogwas=T)
+  write.table(out[[1]], paste0('simulation/simData_', item, '.txt'), row.names=F, quote=F)
+  write.table(out[[2]], paste0('simulation/simGWAS_', item, '.txt'), row.names=F, quote=F)
   write.table(arcs, paste0('simulation/simSolution_', item, '.txt'), row.names=F, quote=F)
 }
-zip('simulations.zip', 'simulation')
+#zip('simulations_snp.zip', 'simulation')
+#system('rm -r simulation/')
+define.causal(p, genetic.effectmat)
